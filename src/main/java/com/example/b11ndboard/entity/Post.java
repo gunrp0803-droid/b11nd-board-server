b9f3e5568344104d52823819299947cf780da9f8
dgsw50 <gunrp08031@gmail.com>
2026-06-01 21:46:46 +0900

    feat

---
+++ src/main/java/com/example/b11ndboard/entity/Post.java
@@ -0,0 +1,46 @@
+package com.example.b11ndboard.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    //JWT 완성하기 전까지는 임시로 숫자를 넣거나 static하게 검증할 작성자 ID
+    @Column(nullable = false)
+    private Long userId;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Post(String title, String content, Long userId) {
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
