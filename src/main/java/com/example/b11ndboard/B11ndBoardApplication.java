bb0a461c0d38fe1aab13a68ff26aa61ecf9396cf
dgsw50 <gunrp08031@gmail.com>
2026-06-01 19:28:35 +0900

    Initial commit

---
+++ src/main/java/com/example/b11ndboard/B11ndBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.b11ndboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B11ndBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B11ndBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/b11ndboard/B11ndBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b11ndboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B11ndBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
