3947ae2cdc8ef8ed4a2bee1f782146cc69501202
kimjaeyun124 <kimjaeyun124@gmail.com>
2026-07-15 19:04:20 +0900

    refactor: DTO를 record에서 class로 변환

--- src/main/java/com/example/b11ndboard/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/example/b11ndboard/domain/auth/dto/request/LoginRequest.java
@@ -1,12 +1,19 @@
 package com.example.b11ndboard.domain.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record LoginRequest(
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class LoginRequest {
 
-        @NotBlank(message = "아이디를 입력해주세요.")
-        String username,
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String username;
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        String password
-) { }
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
